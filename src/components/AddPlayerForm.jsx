fb78c5dd7ccf66862527d60cda9a382ed6faf91c
eastflag <eastflag@gmail.com>
2020-09-08 02:55:25 +0900

    AddPlayerForm.jsx - validation and createRef()

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -6,12 +6,29 @@ class AddPlayerForm extends Component {
     value: ''
   };
 
+  constructor() {
+    super();
+    this.formRef = React.createRef();
+    this.textRef = React.createRef();
+  }
+
   handleValueChange = (e) => {
     this.setState({value: e.target.value});
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const form = this.formRef.current; // form node
+    const player = this.textRef.current; // input node
+
+    console.log(form.checkValidity());
+    console.log(player.validity.valid);
+
+    if (!form.checkValidity()) {
+      return;
+    }
+
     this.props.addPlayer(this.state.value);
     this.setState({
       value: ''
@@ -20,9 +37,10 @@ class AddPlayerForm extends Component {
 
   render() {
     return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <input className="input" type="text" placeholder="enter a player's name"
-               value={this.state.value} onChange={this.handleValueChange}></input>
+      <form ref={this.formRef} className="form" onSubmit={this.handleSubmit} noValidate>
+        <input ref={this.textRef} className="input" type="text" placeholder="enter a player's name"
+               value={this.state.value} onChange={this.handleValueChange}
+               required></input>
         <input className="input" type="submit" value="Add Player"></input>
       </form>
     )
