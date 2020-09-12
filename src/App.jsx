7692a192b108e2b1086d5fa1b51a63ccc745fd11
eastflag <eastflag@gmail.com>
2020-09-12 15:54:33 +0900

    hook 적용 - App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from "./components/Header";
 import AddPlayerForm from "./components/AddPlayerForm";
 import _ from "lodash";
 import {CustomPlayer} from "./components/CustomPlayer";
 
-class App extends React.Component {
-  state = {
-    players: [
-      {name: 'LDK', score: 0, id: 1},
-      {name: 'HONG', score: 0, id: 2},
-      {name: 'KIM', score: 0, id: 3},
-      {name: 'PARK', score: 0, id: 4},
-    ]
-  };
+const App = () => {
+  const [players, setPlayers] = useState([
+    {name: 'LDK', score: 0, id: 1},
+    {name: 'HONG', score: 0, id: 2},
+    {name: 'KIM', score: 0, id: 3},
+    {name: 'PARK', score: 0, id: 4},
+  ]);
 
-  handleRemovePlayer = (id) => {
-    this.setState(prevState => {
-      return {
-        players: prevState.players.filter(item => item.id !== id)
-      }
-    })
+  const  handleRemovePlayer = (id) => {
+    setPlayers(players.filter(item => item.id !== id));
   }
 
-  handleChangeScore = (id, delta) => {
+  const handleChangeScore = (id, delta) => {
     console.log('id: ' + id, 'delta: ' + delta);
-    this.setState(prevState => {
-      const players = [ ...prevState.players ];
-      players.forEach(player => {
-        if (player.id === id) {
-          player.score += delta;
-        }
-      })
-      return { players }
-    })
+    players.forEach(player => {
+      if (player.id === id) {
+        player.score += delta;
+      }
+    });
+    setPlayers([ ...players ]);
   }
 
-  handleAddPlayer = (name) => {
-    this.setState(prevState => {
-      const players = [ ... prevState.players ];
-
-      const maxObject = _.maxBy(players, 'id');
-      const maxId = maxObject.id + 1;
-      console.log(maxId);
-      players.unshift({id: maxId, name, score: 0});
-
-      return { players };
-    });
+  const handleAddPlayer = (name) => {
+    const maxObject = _.maxBy(players, 'id');
+    const maxId = maxObject.id + 1;
+    console.log(maxId);
+    players.unshift({id: maxId, name, score: 0});
+    setPlayers([ ...players ]);
   };
 
-  getHighScore() {
-    const maxObject = _.maxBy(this.state.players, 'score');
+  const getHighScore = () => {
+    const maxObject = _.maxBy(players, 'score');
     const highScore = maxObject.score;
     // 0은 디폴트이므로  0보다 클 경우만 highScore로 지정한다.
     return highScore > 0 ? highScore : null;
   }
 
-  render() {
-    return (
-      <div className="scoreboard">
-        <Header title="My scoreboard" players={this.state.players} />
+  return (
+    <div className="scoreboard">
+      <Header title="My scoreboard" players={players} />
 
-        {/*Players List*/}
-        { this.state.players.map(item => <CustomPlayer key={item.id.toString()}
-                                           id={item.id}
-                                           name={item.name}
-                                           score={item.score}
-                                           isHighScore={item.score === this.getHighScore()}
-                                           removePlayer={this.handleRemovePlayer}
-                                           changeScore={this.handleChangeScore}
-                                         />)
-        }
+      {/*Players List*/}
+      { players.map(item => <CustomPlayer key={item.id.toString()}
+                                                     id={item.id}
+                                                     name={item.name}
+                                                     score={item.score}
+                                                     isHighScore={item.score === getHighScore()}
+                                                     removePlayer={handleRemovePlayer}
+                                                     changeScore={handleChangeScore}
+      />)
+      }
 
-        <AddPlayerForm addPlayer={this.handleAddPlayer} />
-      </div>
-    );
-  }
+      <AddPlayerForm addPlayer={handleAddPlayer} />
+    </div>
+  );
 }
 
 export default App;
